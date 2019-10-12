80a9c4fc9dd3c29362635996a027f49226d3727e
claradios <clara.dios.diez@gmail.com>
2019-10-12 20:17:52 +0200

    add main html structure

--- src/App.js
+++ src/App.js
@@ -9,9 +9,35 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
+        <header className="App__header">
         </header>
+        <main className="App__main">
+          <section className="Main__section_works">
+            <ul className="works__list">
+              <li className="work__project">
+                <h4 className="work__title">titulo del proyecto</h4>
+                <div className="work__picture"><a href="#"><img alt="" src=""/></a></div>                
+              </li>
+              {/* <li className="work__project"></li>
+              <li className="work__project"></li>
+              <li className="work__project"></li>
+              <li className="work__project"></li>
+              <li className="work__project"></li> */}
+            </ul>
+          </section>
+          <section className="Main__section_other">
+          <li className="other__project"></li>
+          <li className="other__project"></li>
+          <li className="other__project"></li>
+          </section>
+        </main>
+        <footer className="App__footer">
+          <ul className="contact__icons-list">
+            <li className="contact__linkedin"></li>
+            <li className="contact__github"></li>
+            <li className="contact__instagram"></li>
+          </ul>
+        </footer>
       </div>
     );
   }
