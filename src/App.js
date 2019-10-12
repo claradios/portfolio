9140b50d785feb822cd4790b3177692bc1e81d62
claradios <clara.dios.diez@gmail.com>
2019-10-12 20:04:50 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
