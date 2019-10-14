9fe4acc2c2bc585a0fa8bef116847ccbf292ad20
claradios <clara.dios.diez@gmail.com>
2019-10-14 14:15:45 +0200

    boder radius to portfolio preview

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,12 +17,12 @@ const projects = [
         url:"http://beta.adalab.es/g-m3-splice-girls/",
         image:"http://via.placeholder.com/250",      
     },
-    {
-        name:"juego adivinar número",
-        description:"esto es una web maquetada con lilili en la que se hace lalala",
-        url:"http://github.com/Adalab/m2-evaluacion-intermedia-claradios",
-        image:"http://via.placeholder.com/250",      
-    },
+    // {
+    //     name:"juego adivinar número",
+    //     description:"esto es una web maquetada con lilili en la que se hace lalala",
+    //     url:"http://github.com/Adalab/m2-evaluacion-intermedia-claradios",
+    //     image:"http://via.placeholder.com/250",      
+    // },
     {
         name:"proyecto kairos",
         description:"esto es una web maquetada con lilili en la que se hace lalala",
