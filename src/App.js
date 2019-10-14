f715003d45267212a123867d105a966bcee96cec
claradios <clara.dios.diez@gmail.com>
2019-10-14 13:39:16 +0200

    fix links and sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss';
 import projects from './components/projects';
+import { isAbsolute } from 'path';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,21 +16,32 @@ class App extends React.Component {
           <h2>front-end developer</h2>
         </header>
         <main className="App__main">
+          <section className="Main__section_about-me">
+            <div className="about-me__container">
+              <h3 className="about-me__title">sobre mi</h3>
+              <p className="about-me__text">
+                La arquitectura del futuro es digital y es ahí donde quiero seguir traspasando mis habilidades técnicas y de diseño.
+              </p>
+              <p className="about-me__text">
+                ¿Quieres ver alguno de mis últimos proyectos de código?
+              </p>
+            </div>
+          </section>
           <section className="Main__section_works">
             <ul className="works__list">
-              {projects.map(project=>
-              <li>
-                <div>
-                  <h4>{project.name}</h4>
-                  <div className="work__picture"><a href={project.url}><img alt="" src={project.image}/></a></div> 
-                </div>
-              </li>)}             
+              {projects.map(project =>
+                <li className="works__item">
+                  <div>
+                    <h4>{project.name}</h4>
+                    <div className="work__picture-ct"><a href={project.url}><img className="work__picture" alt="" src={project.image} /></a></div>
+                  </div>
+                </li>)}
             </ul>
           </section>
           <section className="Main__section_other">
-          <li className="other__project"></li>
-          <li className="other__project"></li>
-          <li className="other__project"></li>
+            <li className="other__project"></li>
+            <li className="other__project"></li>
+            <li className="other__project"></li>
           </section>
         </main>
         <footer className="App__footer">
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,13 +2,13 @@ const projects = [
     {
         name:"web de empresa",
         description:"esto es una web maquetada con lilili en la que se hace lalala",
-        url:"http://beta.adalab.es/g-m3-splice-girls/",
+        url:"http://github.com/Adalab/g-m1-divqueens",
         image:"http://via.placeholder.com/250",      
     },
     {
         name:"tarjetas de contacto digitales",
         description:"esto es una web maquetada con lilili en la que se hace lalala",
-        url:"http://beta.adalab.es/g-m3-splice-girls/",
+        url:"http://github.com/Adalab/g-m2-letmecode",
         image:"http://via.placeholder.com/250",      
     },
     {
@@ -20,19 +20,25 @@ const projects = [
     {
         name:"juego adivinar número",
         description:"esto es una web maquetada con lilili en la que se hace lalala",
-        url:"http://beta.adalab.es/g-m3-splice-girls/",
+        url:"http://github.com/Adalab/m2-evaluacion-intermedia-claradios",
         image:"http://via.placeholder.com/250",      
     },
     {
         name:"proyecto kairos",
         description:"esto es una web maquetada con lilili en la que se hace lalala",
-        url:"http://beta.adalab.es/g-m3-splice-girls/",
+        url:"http://github.com/Adalab/team-kairos",
         image:"http://via.placeholder.com/250",      
     },
     {
-        name:"juego de pokemon",
+        name:"buscador de series",
         description:"esto es una web maquetada con lilili en la que se hace lalala",
-        url:"http://beta.adalab.es/g-m3-splice-girls/",
+        url:"http://github.com/Adalab/m2-evaluacion-final-claradios",
+        image:"http://via.placeholder.com/250",      
+    },
+    {
+        name:"buscador de ricky y morty",
+        description:"esto es una web maquetada con lilili en la que se hace lalala",
+        url:"http://github.com/Adalab/m3-evaluacion-final-claradios",
         image:"http://via.placeholder.com/250",      
     }
 ]
