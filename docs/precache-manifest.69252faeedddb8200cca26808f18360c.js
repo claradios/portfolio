bd5f926ac52c5bdcbece75b0d88e373adf26995c
claradios <clara.dios.diez@gmail.com>
2019-10-30 20:36:41 +0100

    create bulid docs

---
+++ docs/precache-manifest.69252faeedddb8200cca26808f18360c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5227787693ce85390bbaa2e50851764c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce7694e35d4f5ff91d91",
+    "url": "./static/css/main.088549b8.chunk.css"
+  },
+  {
+    "revision": "be94dd2216a874a68fe7",
+    "url": "./static/js/2.173b9dff.chunk.js"
+  },
+  {
+    "revision": "ce7694e35d4f5ff91d91",
+    "url": "./static/js/main.19de5f95.chunk.js"
+  },
+  {
+    "revision": "dda1c863fd1c52192d0c",
+    "url": "./static/js/runtime-main.c5db20d9.js"
+  },
+  {
+    "revision": "61a85a2b1c84f811a5772506b788b79c",
+    "url": "./static/media/icono01.61a85a2b.svg"
+  },
+  {
+    "revision": "39f4837bea787aea3a379370d9226d2d",
+    "url": "./static/media/icono02.39f4837b.svg"
+  },
+  {
+    "revision": "6dc64f16c24a8ed0b27e701abc2d5385",
+    "url": "./static/media/icono03.6dc64f16.svg"
+  },
+  {
+    "revision": "d7726c797bac33f6982fad7add2476b2",
+    "url": "./static/media/icono04.d7726c79.svg"
+  },
+  {
+    "revision": "29ef3980c66ea944b57134fb7cd9f1cd",
+    "url": "./static/media/icono05.29ef3980.svg"
+  },
+  {
+    "revision": "f44db197ef7f54a778bf8c1d36c3e8f0",
+    "url": "./static/media/icono06.f44db197.svg"
+  }
+]);
